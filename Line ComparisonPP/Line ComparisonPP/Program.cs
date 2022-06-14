196d801c4f2907900fd0cf834601e1468cb38186
tutorial6 <manapure.vaibhav542@gmail.com>
2022-06-14 18:12:28 +0530

    Add UC1-CartesianSystem

---
+++ Line ComparisonPP/Line ComparisonPP/Program.cs
@@ -0,0 +1,15 @@
+﻿using LineComparisonProblem;
+using System;
+
+namespace Line_ComparisonPP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Welcome to Line Comparison Computation Program");
+
+            CartesianSystem.Calculatinglength();
+        }
+    }
+}
